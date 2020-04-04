0de125450579863b8f4a9ab8ae45d2b0fab53227
inesms7 <43278764+inesms7@users.noreply.github.com>
2020-04-04 15:59:05 +0100

    Create forecast temperature css

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,17 +1,64 @@
 import React from "react";
 
-
-export default function Forecast(){
-    return (
-			<div className="forecast">
-				<div className="forecast-container">
-					<div className="forecast-column">One</div>
-					<div className="forecast-column">Two</div>
-					<div className="forecast-column">Three</div>
-					<div className="forecast-column">Four</div>
-					<div className="forecast-column">Five</div>
-					<div className="forecast-column">Six</div>
+export default function Forecast() {
+	return (
+		<div className="forecast">
+			<div className="forecast-container">
+				<div className="forecast-column">26 April</div>
+				<div className="forecast-column">27 April</div>
+				<div className="forecast-column">28 April</div>
+				<div className="forecast-column">29 April</div>
+				<div className="forecast-column">30 April</div>
+				<div className="forecast-column">1 May</div>
+				<div className="forecast-temperature">10º</div>
+				<div className="forecast-temperature">12º</div>
+				<div className="forecast-temperature">9º</div>
+				<div className="forecast-temperature">14º</div>
+				<div className="forecast-temperature">18º</div>
+				<div className="forecast-temperature">20º</div>
+				<div className="forecast-icon">
+					<img
+						src="https://i.dlpng.com/static/png/117769_preview.png"
+						alt=""
+						width="60px"
+					/>
+				</div>
+				<div className="forecast-icon">
+					<img
+						src="https://i.dlpng.com/static/png/117769_preview.png"
+						alt=""
+						width="60px"
+					/>
+				</div>
+				<div className="forecast-icon">
+					<img
+						src="https://i.dlpng.com/static/png/117769_preview.png"
+						alt=""
+						width="60px"
+					/>
+				</div>
+				<div className="forecast-icon">
+					<img
+						src="https://i.dlpng.com/static/png/117769_preview.png"
+						alt=""
+						width="60px"
+					/>
+				</div>
+				<div className="forecast-icon">
+					<img
+						src="https://i.dlpng.com/static/png/117769_preview.png"
+						alt=""
+						width="60px"
+					/>
+				</div>
+				<div className="forecast-icon">
+					<img
+						src="https://i.dlpng.com/static/png/117769_preview.png"
+						alt=""
+						width="60px"
+					/>
 				</div>
 			</div>
-		);
+		</div>
+	);
 }
