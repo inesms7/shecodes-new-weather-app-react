9fcf6b9805e266380d24037d572dcfc3dc7b5ccf
inesms7 <43278764+inesms7@users.noreply.github.com>
2020-04-04 17:03:22 +0100

    Create Date & Time

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "./App.css";
 
+export default function SearchEngine() {
+	return (
+		<div>
+            <p className="date">4 de Abril, 2020 | <span className="time"> 10:00</span></p>
+			<form className="search-form">
+				<input type="text" className="form-input" />
+				<button type="submit" className="submit-form">
+					Search
+				</button>
+			</form>
 
-export default function SearchEngine(){
-    return (
-        <form className="search-form">
-            <input type="text" className="form-input"/>
-            <button type="submit" className="submit-form">Search</button>
-        </form>
-    )
+            <button className="location">Current Location</button>
+		</div>
+	);
 }
-
-
