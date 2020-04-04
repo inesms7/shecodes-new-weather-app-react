a7cddca7496cc96c964c31579862c98bde63950e
inesms7 <43278764+inesms7@users.noreply.github.com>
2020-04-04 14:52:08 +0100

    Create Actual temperature with a Grid

--- src/App.js
+++ src/App.js
@@ -2,12 +2,29 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-	return <div className="App">
-    <div className="wrapper">
-      <div className="container">
-        <h1>Lisbon</h1>
-        <p>Weather App</p>
-      </div>
-    </div>
-  </div>;
+	return (
+		<div className="App">
+			<div className="wrapper">
+				<div className="container">
+					<div className="column-one column">
+						<img src="" alt="" />
+					</div>
+					<div className="column-two column">
+						<h2 className="temperature">15 <span>º</span></h2>
+					</div>
+          <div className="column-third column">
+            <h1>Lisbon</h1>	
+					</div>
+					<div className="column-fourthc column">
+            <ul className="weather-conditions">
+							<li>Wind</li>
+							<li>Precipitation</li>
+							<li>Real Feel</li>
+						</ul>One</div>
+					<div className="column-fifth column">One</div>
+					<div className="column-six column">One</div>
+				</div>
+			</div>
+		</div>
+	);
 }
