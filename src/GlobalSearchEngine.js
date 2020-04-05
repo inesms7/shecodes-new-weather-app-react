3993f2dcd1e28832a3e4bf72d1c348b92db73bcc
inesms7 <43278764+inesms7@users.noreply.github.com>
2020-04-05 17:09:17 +0100

    Update Today's Date

--- src/GlobalSearchEngine.js
+++ src/GlobalSearchEngine.js
@@ -1,73 +1,81 @@
 import React, { useState } from "react";
 import "./App.css";
 import "./GlobalSearchEngine.css";
+import UpdatedDate from "./UpdatedDate";
 import axios from "axios";
 
 export default function GlobalSearchEngine() {
-    const [weatherinfo, showWeatherInfo] = useState({ ready:false});
+	const [weatherinfo, showWeatherInfo] = useState({ ready: false });
 
-    function showResponse(response){
-    showWeatherInfo({
+	function showResponse(response) {
+		showWeatherInfo({
 			ready: true,
 			temperature: response.data.main.temp,
 			humidity: response.data.main.humidity,
 			wind: response.data.wind.speed,
 			city: response.data.name,
-			date: newDate(response.data.dt * 1000),
+			date: new Date(response.data.dt * 1000),
 			description: response.data.weather[0].description,
 			icon: response.data.weather[0].icon,
 		});
-    }
+	}
 
-    if(weatherinfo.ready){
-        return(
-            <div className="container">
-                <div className="column-one column">
-                    <img
-                        src={weatherinfo.icon}
-                        alt={weatherinfo.description}
-                        width="80%"
-                        className="sun-img"
-                    />
-                </div>
-                <div className="column-two column">
-                    <h2 className="temperature">
-                        {Math.round(weatherinfo.temperature)}{" "}
-                        <span className="metrics">
-                            º<a href="#">C</a> | <a href="#">F</a>
-                        </span>
-                    </h2>
-                </div>
-                <div className="column-third column">
-                    <h1>{weatherinfo.city}</h1>
-                    <ul className="weather-conditions">
-                        <li>Wind: {weatherinfo.wind} </li>
-                        <li>Humidity: {weatherinfo.humidity} </li>
-                        <li>Description: {weatherinfo.description} </li>
-                    </ul>
-                </div>
-                <div className="column-fourth column">
-                    <div>
-                        <p className="date">
-                           <Date date={weatherinfo.date}/> | <span className="time"> 10:00</span>
-                        </p>
-                        <form className="search-form">
-                            <input type="search" className="form-input" />
-                            <input type="submit" className="submit-form" value="Search" />
-                        </form>
+	if (weatherinfo.ready) {
+		return (
+			<div className="container">
+				<div className="column-one column">
+					<img
+						src={weatherinfo.icon}
+						alt={weatherinfo.description}
+						width="80%"
+						className="sun-img"
+					/>
+				</div>
+				<div className="column-two column">
+					<h2 className="temperature">
+						{Math.round(weatherinfo.temperature)}{" "}
+						<span className="metrics">
+							º<a href="#">C</a> | <a href="#">F</a>
+						</span>
+					</h2>
+				</div>
+				<div className="column-third column">
+					<h1>{weatherinfo.city}</h1>
+					<ul className="weather-conditions">
+						<li>
+							<span className="description">Wind:</span> {weatherinfo.wind} km/h{" "}
+						</li>
+						<li>
+							<span className="description">Humidity:</span>{" "}
+							{weatherinfo.humidity} %{" "}
+						</li>
+						<li>
+							<span className="description">Description:</span>{" "}
+							{weatherinfo.description}{" "}
+						</li>
+					</ul>
+				</div>
+				<div className="column-fourth column">
+					<div>
+						<p className="date">
+							<UpdatedDate date={weatherinfo.date} />
+						</p>
+						<form className="search-form">
+							<input type="search" className="form-input" />
+							<input type="submit" className="submit-form" value="Search" />
+						</form>
 
-                        <button className="location">Current Location</button>
-                    </div>
-                </div>
-            </div>)
-        } else {
-        
-        let apiKey = "3cfbc7eebafcf9149917ab5969c53e6c";
-        let city = "Lisbon";
-        let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-        axios.get(weatherUrl).then(showResponse);
+						<button className="location">Current Location</button>
+					</div>
+				</div>
+			</div>
+		);
+	} else {
+		let apiKey = "3cfbc7eebafcf9149917ab5969c53e6c";
+		let city = "Lisbon";
+		let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+		axios.get(weatherUrl).then(showResponse);
 
-        return("Loading...")
-
-        } 
+		return "Loading...";
+	}
 }
---
+++ src/UpdatedDate.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+export default function UpdatedDate(props) {
+	let days = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
+
+	let Tday = props.date.getDate();
+	let day = days[props.date.getDay()];
+	let hours = props.date.getHours();
+	let minutes = props.date.getMinutes();
+
+	if (minutes < 10) {
+		minutes = `0${minutes}`;
+	}
+
+	if (hours < 10) {
+		hours = `0${hours}`;
+	}
+
+	let months = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	];
+
+	let month = months[props.date.getMonth()];
+
+	return (
+		<div>
+			{day}, {hours}:{minutes} <strong> | </strong> {Tday} of {month}
+		</div>
+	);
+}
