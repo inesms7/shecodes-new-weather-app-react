ce56f2b826b8aad885d5b2bad28f05ec16933860
inesms7 <43278764+inesms7@users.noreply.github.com>
2020-04-05 18:12:46 +0100

    Create Api request for city input

--- src/GlobalSearchEngine.js
+++ src/GlobalSearchEngine.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import "./App.css";
 import "./GlobalSearchEngine.css";
-import UpdatedDate from "./UpdatedDate";
+import Temperature from "./Temperature";
 import axios from "axios";
 
-export default function GlobalSearchEngine() {
+export default function GlobalSearchEngine(props) {
 	const [weatherinfo, showWeatherInfo] = useState({ ready: false });
+	const [city, setCity] = useState(" ");
 
 	function showResponse(response) {
 		showWeatherInfo({
@@ -16,66 +17,39 @@ export default function GlobalSearchEngine() {
 			city: response.data.name,
 			date: new Date(response.data.dt * 1000),
 			description: response.data.weather[0].description,
-			icon: response.data.weather[0].icon,
+			icon: `http://openweathermap.org/img/wn${response.data.weather[0].icon}`,
 		});
+	}
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		search();
+	}
+
+	function findCity(event) {
+		setCity(event.target.value);
+	}
+
+	function search() {
+		/*let apiKey = "3cfbc7eebafcf9149917ab5969c53e6c";
+		let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+		axios.get(weatherUrl).then(showResponse);*/
 	}
 
 	if (weatherinfo.ready) {
 		return (
-			<div className="container">
-				<div className="column-one column">
-					<img
-						src={weatherinfo.icon}
-						alt={weatherinfo.description}
-						width="80%"
-						className="sun-img"
-					/>
-				</div>
-				<div className="column-two column">
-					<h2 className="temperature">
-						{Math.round(weatherinfo.temperature)}{" "}
-						<span className="metrics">
-							º<a href="#">C</a> | <a href="#">F</a>
-						</span>
-					</h2>
-				</div>
-				<div className="column-third column">
-					<h1>{weatherinfo.city}</h1>
-					<ul className="weather-conditions">
-						<li>
-							<span className="description">Wind:</span> {weatherinfo.wind} km/h{" "}
-						</li>
-						<li>
-							<span className="description">Humidity:</span>{" "}
-							{weatherinfo.humidity} %{" "}
-						</li>
-						<li>
-							<span className="description">Description:</span>{" "}
-							{weatherinfo.description}{" "}
-						</li>
-					</ul>
-				</div>
-				<div className="column-fourth column">
-					<div>
-						<p className="date">
-							<UpdatedDate date={weatherinfo.date} />
-						</p>
-						<form className="search-form">
-							<input type="search" className="form-input" />
-							<input type="submit" className="submit-form" value="Search" />
-						</form>
+			<div className="column-fourth column">
+				<Temperature weather={weatherinfo} />
+				<form className="search-form" onSubmit={handleSubmit}>
+					<input type="search" className="form-input" onChange={findCity} />
+					<input type="submit" className="submit-form" value="Search" />
 
-						<button className="location">Current Location</button>
-					</div>
-				</div>
+					<button className="location">Current Location</button>
+				</form>
 			</div>
 		);
 	} else {
-		let apiKey = "3cfbc7eebafcf9149917ab5969c53e6c";
-		let city = "Lisbon";
-		let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-		axios.get(weatherUrl).then(showResponse);
-
-		return "Loading...";
+		search();
+		return "Searching...";
 	}
 }
--- src/Temperature.js
+++ src/Temperature.js
@@ -1,31 +1,49 @@
-/*import React, { useState } from "react";
+import React from "react";
+import UpdatedDate from "./UpdatedDate";
 
 export default function Temperature(props) {
-	const [temperature, setTemperature] = useState(props.temp);
-
-	function showFahrenheit(event) {
-		event.preventDefault();
-		setTemperature((props.temp * 9) / 5 + 35);
-	}
-
-	function showCelsius(event) {
-		event.preventDefault();
-		setTemperature(((props.temp - 32) * 5) / 9);
-	}
 
 	return (
-		<h2 className="temperature">
-			{temperature}{" "}
-			<span className="metrics">
-				º
-				<a href="#" onClick={showCelsius}>
-					C
-				</a>{" "}
-				|{" "}
-				<a href="#" onClick={showFahrenheit}>
-					F
-				</a>
-			</span>
-		</h2>
+		<div className="container">
+			<div className="column-one column">
+				<img
+					src={props.weather.icon}
+					alt={props.weather.description}
+					width="80%"
+					className="sun-img"
+				/>
+			</div>
+			<div className="column-two column">
+				<h2 className="temperature">
+					{Math.round(props.weather.temperature)}{" "}
+					<span className="metrics">
+						º<a href="#">C</a> | <a href="#">F</a>
+					</span>
+				</h2>
+			</div>
+			<div className="column-third column">
+				<h1>{props.weather.city}</h1>
+				<ul className="weather-conditions">
+					<li>
+						<span className="description">Wind:</span> {props.weather.wind} km/h{" "}
+					</li>
+					<li>
+						<span className="description">Humidity:</span>{" "}
+						{props.weather.humidity} %{" "}
+					</li>
+					<li>
+						<span className="description">Description:</span>{" "}
+						{props.weather.description}{" "}
+					</li>
+				</ul>
+			</div>
+			<div className="column-fourth column">
+				<div>
+					<p className="date">
+						<UpdatedDate date={props.weather.date} />
+					</p>
+				</div>
+			</div>
+		</div>
 	);
 }
